a36f8a78f8c4186f77504c8467427c3ae55d0969
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-10-14 16:01:56 +0100

    Initialise Unit Test Project

---
+++ BattleShips.Game.API.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BattleShips.Game.API.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
