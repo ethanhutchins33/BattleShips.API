e3a62310ce104b8ab5eec18c08fc1ef6b90da9f1
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-11-08 10:56:04 +0000

    WIP setup everything before Db migration scripts created

---
+++ BattleShips.Game.API.Data/DataAccess/GameContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BattleShips.Game.API.Data.Models;
+
+namespace BattleShips.Game.API.Data.DataAccess;
+public class GameContext : DbContext
+{
+    public GameContext(DbContextOptions<GameContext> options) : base(options) { }
+
+    public DbSet<Models.Game> Games { get; set; }
+    public DbSet<Board> Boards { get; set; }
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Ship> Ships { get; set; }
+    public DbSet<ShipType> ShipTypes { get; set; }
+}
--- BattleShips.Game.API.Data/Models/Game.cs
+++ BattleShips.Game.API.Data/Models/Game.cs
@@ -1,18 +1,17 @@
-﻿namespace BattleShips.Game.API.Data.Models
-{
-    public class Game
-    {
-        public int Id { get; set; }
-        public DateTime DateCreated { get; set; }
-        public Player Player1Id { get; set; }
-        public Player Player2Id { get; set; }
-        public int PlayerWinnerId { get; set; }
+﻿namespace BattleShips.Game.API.Data.Models;
 
-        public Game()
-        {
-            DateCreated = DateTime.Now;
-            Player1Id = new Player();
-            Player2Id = new Player();
-        }
+public class Game
+{
+    public int Id { get; set; }
+    public DateTime DateCreated { get; set; }
+    public Player Player1Id { get; set; }
+    public Player Player2Id { get; set; }
+    public int PlayerWinnerId { get; set; }
+
+    public Game()
+    {
+        DateCreated = DateTime.Now;
+        Player1Id = new Player();
+        Player2Id = new Player();
     }
 }
