ce6a4fa5db9204bbeb2d2837ef4b67aff5120c70
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-12-25 14:32:57 +0000

    Pulled in playercontroller and playerservice from bs.users.api

---
+++ BattleShips.API.Library/IPlayerService.cs
@@ -0,0 +1,11 @@
+﻿using BattleShips.API.Data.Models;
+
+namespace BattleShips.API.Library;
+
+public interface IPlayerService
+{
+    Player? Get(Guid azureId);
+    Task<Player?> Get(int id);
+    Task<Player?> Add(Guid azureId);
+    Task<Player?> Remove(int id);
+}
