4e12a0d6c05872a15b3ae6e026aa5abb6becef48
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-01-11 16:44:52 +0000

    comment out game controller, add back in services for playercontroller

--- BattleShips.API/Controllers/GameController.cs
+++ BattleShips.API/Controllers/GameController.cs
@@ -1,131 +1,131 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using BattleShips.API.Library;
-using BattleShips.API.Library.Requests;
-using BattleShips.API.Library.Responses;
-using Microsoft.AspNetCore.Authorization;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using BattleShips.API.Library;
+//using BattleShips.API.Library.Requests;
+//using BattleShips.API.Library.Responses;
+//using Microsoft.AspNetCore.Authorization;
 
-namespace BattleShips.API.Controllers;
+//namespace BattleShips.API.Controllers;
 
-[Authorize]
-[ApiController]
-[Route("api/game")]
-public class GameController : ControllerBase
-{
-    private readonly IGameService _gameService;
-    private readonly IPlayerService _playerService;
+//[Authorize]
+//[ApiController]
+//[Route("api/game")]
+//public class GameController : ControllerBase
+//{
+//    private readonly IGameService _gameService;
+//    private readonly IPlayerService _playerService;
 
-    public GameController(IGameService gameService, IPlayerService playerService)
-    {
-        _gameService = gameService;
-        _playerService = playerService;
-    }
+//    public GameController(IGameService gameService, IPlayerService playerService)
+//    {
+//        _gameService = gameService;
+//        _playerService = playerService;
+//    }
 
-    [HttpPost]
-    [Route("create")]
-    public async Task<ActionResult<CreateGameResponseDto>> CreateGame()
-    {
-        //TODO validate dto
-        var azureId = HttpContext.User.Claims.Single(c => c.Type == "sub").Value;
-        var newGame = await _gameService.SetupNewGameAsync(_playerService.Get(Guid.Parse(azureId)).Id);
+//    [HttpPost]
+//    [Route("create")]
+//    public async Task<ActionResult<CreateGameResponseDto>> CreateGame()
+//    {
+//        //TODO validate dto
+//        var azureId = HttpContext.User.Claims.Single(c => c.Type == "sub").Value;
+//        var newGame = await _gameService.SetupNewGameAsync(_playerService.Get(Guid.Parse(azureId)).Id);
 
-        if (newGame != null)
-        {
-            return Ok(new CreateGameResponseDto { GameId = newGame.Id, GameCode = newGame.GameCode });
-        }
-        return BadRequest();
-    }
-
-
-    //TODO JOIN GAME
-    [HttpPost]
-    [Route("join/{gameCode}")]
-    public async Task<ActionResult<JoinGameResponseDto>> JoinGame(string gameCode)
-    {
-        var azureId = HttpContext.User.Claims.Single(c => c.Type == "sub").Value;
-        var player = _playerService.Get(Guid.Parse(azureId));
-        var game = _gameService.GetGameByGameCode(gameCode);
+//        if (newGame != null)
+//        {
+//            return Ok(new CreateGameResponseDto { GameId = newGame.Id, GameCode = newGame.GameCode });
+//        }
+//        return BadRequest();
+//    }
 
 
-        var gameToReturn = await _gameService.AddPlayerToGameAsync(player.Id, game.Id);
-        var boardToReturn = await _gameService.NewBoardAsync(player.Id, game.Id);
-
-        var opponentId = await _gameService.GetOpponentIdAsync(player.Id, game.Id);
-        var opponentBoardId = _gameService.GetOpponentBoardId(opponentId, game.Id);
-
-        if (gameToReturn == null)
-        {
-            return NoContent();
-        }
-
-        if (boardToReturn == null)
-        {
-            return NoContent();
-        }
-
-        return Ok(new JoinGameResponseDto
-        {
-            GameId = gameToReturn.Id,
-            GameCode = gameCode,
-            BoardId = boardToReturn.Id,
-            PlayerId = player.Id,
-            OpponentPlayerId = opponentId,
-            OpponentBoardId = opponentBoardId,
-        });
-
-    }
-
-    //TODO ADD SHIPS
-    [HttpPost]
-    [Route("addships")]
-    public async Task<ActionResult<AddShipsResponseDto>> AddPlayerShips(AddShipsRequestDto addShips)
-    {
-
-        await _gameService.AddShipsToBoardAsync(addShips.Board, addShips.GameCode, addShips.PlayerId);
-
-        return Ok(
-            new AddShipsResponseDto 
-            { 
-                GameCode = addShips.GameCode, 
-                PlayerId = addShips.PlayerId
-            });
-    }
-
-    //TODO START GAME (after 5 ships in each board)
-
-    //TODO FIRE AT SHIPS
-    [HttpPost]
-    [Route("fire")]
-    public ActionResult<ShotFiredResponseDto> ShotFired(ShotFiredDto shotFiredDto)
-    {
-        //TODO validate shot and check against ship locations in database
-
-        //var result = _gameService.CheckShot
-        //    (
-        //        shotFiredDto.BoardId,
-        //        shotFiredDto.RowNumber,
-        //        shotFiredDto.CellValue
-        //    );
-
-        Console.WriteLine(shotFiredDto.GameCode);
-        Console.WriteLine(shotFiredDto.BoardId);
-        Console.WriteLine(shotFiredDto.Y);
-        Console.WriteLine(shotFiredDto.X);
-
-        return Ok(new ShotFiredResponseDto 
-        { 
-            ShotResult = "hit"
-        });
-    }
-
-    //TODO GETGAMESTATE => who's turn, winnerBool, list of shots so far
-
-    //[HttpGet("id")]
-    //public async Task<ActionResult<Game>> GetGame(Player id)
-    //{
-    //    return Ok();
-    //}
+//    //TODO JOIN GAME
+//    [HttpPost]
+//    [Route("join/{gameCode}")]
+//    public async Task<ActionResult<JoinGameResponseDto>> JoinGame(string gameCode)
+//    {
+//        var azureId = HttpContext.User.Claims.Single(c => c.Type == "sub").Value;
+//        var player = _playerService.Get(Guid.Parse(azureId));
+//        var game = _gameService.GetGameByGameCode(gameCode);
 
 
+//        var gameToReturn = await _gameService.AddPlayerToGameAsync(player.Id, game.Id);
+//        var boardToReturn = await _gameService.NewBoardAsync(player.Id, game.Id);
+
+//        var opponentId = await _gameService.GetOpponentIdAsync(player.Id, game.Id);
+//        var opponentBoardId = _gameService.GetOpponentBoardId(opponentId, game.Id);
+
+//        if (gameToReturn == null)
+//        {
+//            return NoContent();
+//        }
+
+//        if (boardToReturn == null)
+//        {
+//            return NoContent();
+//        }
+
+//        return Ok(new JoinGameResponseDto
+//        {
+//            GameId = gameToReturn.Id,
+//            GameCode = gameCode,
+//            BoardId = boardToReturn.Id,
+//            PlayerId = player.Id,
+//            OpponentPlayerId = opponentId,
+//            OpponentBoardId = opponentBoardId,
+//        });
+
+//    }
+
+//    //TODO ADD SHIPS
+//    [HttpPost]
+//    [Route("addships")]
+//    public async Task<ActionResult<AddShipsResponseDto>> AddPlayerShips(AddShipsRequestDto addShips)
+//    {
+
+//        await _gameService.AddShipsToBoardAsync(addShips.Board, addShips.GameCode, addShips.PlayerId);
+
+//        return Ok(
+//            new AddShipsResponseDto 
+//            { 
+//                GameCode = addShips.GameCode, 
+//                PlayerId = addShips.PlayerId
+//            });
+//    }
+
+//    //TODO START GAME (after 5 ships in each board)
+
+//    //TODO FIRE AT SHIPS
+//    [HttpPost]
+//    [Route("fire")]
+//    public ActionResult<ShotFiredResponseDto> ShotFired(ShotFiredDto shotFiredDto)
+//    {
+//        //TODO validate shot and check against ship locations in database
+
+//        //var result = _gameService.CheckShot
+//        //    (
+//        //        shotFiredDto.BoardId,
+//        //        shotFiredDto.RowNumber,
+//        //        shotFiredDto.CellValue
+//        //    );
+
+//        Console.WriteLine(shotFiredDto.GameCode);
+//        Console.WriteLine(shotFiredDto.BoardId);
+//        Console.WriteLine(shotFiredDto.Y);
+//        Console.WriteLine(shotFiredDto.X);
+
+//        return Ok(new ShotFiredResponseDto 
+//        { 
+//            ShotResult = "hit"
+//        });
+//    }
+
+//    //TODO GETGAMESTATE => who's turn, winnerBool, list of shots so far
+
+//    //[HttpGet("id")]
+//    //public async Task<ActionResult<Game>> GetGame(Player id)
+//    //{
+//    //    return Ok();
+//    //}
 
 
-}
+
+
+//}
--- BattleShips.API/Program.cs
+++ BattleShips.API/Program.cs
@@ -17,33 +17,33 @@ builder.Services.AddSwaggerGen();
 
 //builder.Services.AddScoped<IRepository<Game>, GameRepository>();
 //builder.Services.AddScoped<IRepository<Board>, BoardRepository>();
-//builder.Services.AddScoped<IRepository<Player>, PlayerRepository>();
+builder.Services.AddScoped<IRepository<Player>, PlayerRepository>();
 //builder.Services.AddScoped<IRepository<Ship>, ShipRepository>();
 //builder.Services.AddScoped<IRepository<ShipType>, ShipTypeRepository>();
 //builder.Services.AddScoped<IGameService, GameService>();
-//builder.Services.AddScoped<IPlayerService, PlayerService>();
+builder.Services.AddScoped<IPlayerService, PlayerService>();
 
 builder.Services.AddHealthChecks();
 
-//builder.Services
-//    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
-//builder.Services.AddDbContext<BattleShipsContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("Db-ConnString"));
-//});
+builder.Services.AddDbContext<BattleShipsContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Db-ConnString"));
+});
 
 var app = builder.Build();
 
-//using (var scope = app.Services.CreateScope())
-//{
-//    var dbContext = scope.ServiceProvider.GetRequiredService<BattleShipsContext>();
-//    //dbContext.Database.EnsureDeleted();
-//    dbContext.Database.EnsureCreated();
-//}
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<BattleShipsContext>();
+    //dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+}
 
 //// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -59,7 +59,7 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-//app.MapControllers();
+app.MapControllers();
 
 app.MapHealthChecks("/health");
 
