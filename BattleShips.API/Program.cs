f67f771978959706463564c308f8a059d3b0c43f
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-01-12 14:58:31 +0000

    Add Migrations

--- BattleShips.API/Program.cs
+++ BattleShips.API/Program.cs
@@ -56,8 +56,9 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<BattleShipsContext>();
-    dbContext.Database.EnsureDeleted();
-    dbContext.Database.EnsureCreated();
+    //dbContext.Database.EnsureDeleted();
+    //dbContext.Database.EnsureCreated();
+    dbContext.Database.Migrate();
 }
 
 if (app.Environment.IsDevelopment())
